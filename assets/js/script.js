4a22a77d994fbcc1cd7e69010d82dcd9c4d89aca
unknown <joseph.harley02@gmail.com>
2023-01-03 18:34:51 -0500

    Added html for index page, and added css for index.html

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var startButtonEL = document.querySelector("#start-button");
+var timerEL = document.querySelector("#countdown")
+
+
+// sends user to html page of the first javascript question
+startButtonEL.addEventListener("click", function() {
+
+});
+
